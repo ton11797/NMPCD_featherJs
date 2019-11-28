0650b179766aaa8c50b062bbae5e37e852f72079
pongsatorn <ton11797@hotmail.com>
2019-11-29 01:05:50 +0700

    test link data

--- test/runall.js
+++ test/runall.js
@@ -3,7 +3,7 @@ const readline = require('readline-sync');
 const API = "http://127.0.0.1:3030"
 const debug = false
 const runall = false
-const random = Math.floor((Math.random() * 100) + 1);
+const random = Math.floor((Math.random() * 1000) + 1);
 console.log(random)
 const register = {
     "email": `test${random}@feathersjs.com`,
@@ -72,33 +72,53 @@ let creatMetaLink = [
     ["TP","TPU"],
     ["TP","STD24"],
 ]
-axios.defaults.baseURL = API;
-async function runTest(){
-    await axios.post('/users',register).then((data)=>{
-            if(debug)console.log(data.data)
-            console.log(">>>>Register Pass")
-        }).catch((data)=>{
-            console.log("XXXXXRegister Fail")
-            console.log(data.response.data)
-        });
-    await axios.post('/authentication',login).then((data)=>{
-            AUTH_TOKEN = data.data.accessToken
-            console.log(">>>>login Pass")
-            console.log(">>>>accessToken ",AUTH_TOKEN)
-        }).catch((data)=>{
-            console.log("XXXXXlogin Fail")
-            console.log(data.response.data)
-        });
 
+let createDataLink =[
+    [0,2] ,
+    [1,3] ,
+    [4,6] ,
+    [5,7] ,
+    [8,10] ,
+    [9,11] ,
+    [4,12],
+    [5,13],
+    [0,4] ,
+    [1,5],
+    [10,0],
+    [11,1]
+]
+axios.defaults.baseURL = API;
+async function Run_register(){
+    await axios.post('/users',register).then((data)=>{
+        if(debug)console.log(data.data)
+        console.log(">>>>Register Pass")
+    }).catch((data)=>{
+        console.log("XXXXXRegister Fail")
+        console.log(data.response.data)
+    });
+}
+async function Run_login(){
+    await axios.post('/authentication',login).then((data)=>{
+        AUTH_TOKEN = data.data.accessToken
+        console.log(">>>>login Pass")
+        console.log(">>>>accessToken ",AUTH_TOKEN)
+    }).catch((data)=>{
+        console.log("XXXXXlogin Fail")
+        console.log(data.response.data)
+    });
     axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+}
+async function Run_createVersion1(){
     await axios.post('/versionControl/new-version',createVersion1).then((data)=>{
-            console.log(">>>>createVersion1 Pass")
-            uuidV1  = data.data.uuid
-            console.log(">>>>uuidV1 ",uuidV1)
-        }).catch((data)=>{
-            console.log("XXXXXcreateVersion1 Fail")
-            console.log(data.response.data)
-        });
+        console.log(">>>>createVersion1 Pass")
+        uuidV1  = data.data.uuid
+        console.log(">>>>uuidV1 ",uuidV1)
+    }).catch((data)=>{
+        console.log("XXXXXcreateVersion1 Fail")
+        console.log(data.response.data)
+    });
+}
+async function Run_createSchema(){
     for(let i =0;i<createSchema.length;i++){
         for(let j=0;j<createSchema[i].fieldName.length;j++){
             let request = {
@@ -118,24 +138,21 @@ async function runTest(){
         console.log(`>>>>createschema ${createSchema[i].schemaName} Pass`)
     }
     console.log(`>>>>createschemaAll Pass`)
-    if(!runall)name = readline.question("Continues?");
-    let realData = "n"
-    if(!runall)realData = readline.question("import real data(y/n)?");
-    if(realData === 'y'){
+}
+async function Run_importTest(){
+    for(let i =0;i<importTest.length;i++){
+        let request = importTest[i]
+        request.versionUUID = uuidV1
+        await axios.post('/data/insert',request).then((data)=>{
 
-    }else{
-        for(let i =0;i<importTest.length;i++){
-            let request = importTest[i]
-            request.versionUUID = uuidV1
-            await axios.post('/data/insert',request).then((data)=>{
-
-            }).catch((data)=>{
-                console.log("XXXXXimportTest data Fail")
-                console.log(data.response.data)
-            });
-        }
-        console.log(`>>>>importTest data  Pass`)
+        }).catch((data)=>{
+            console.log("XXXXXimportTest data Fail")
+            console.log(data.response.data)
+        });
     }
+    console.log(`>>>>importTest data  Pass`)
+}
+async function Run_creatMetaLink(){
     for(let i =0;i<creatMetaLink.length;i++){
         let request = {
             "node1":creatMetaLink[i][0],
@@ -149,8 +166,60 @@ async function runTest(){
             console.log(data.response.data)
         });
     }
+}
+async function Run_TestSearch(){
+    if(uuidV1 === ''){
+        uuidV1 = readline.question("uuidV1:");
+    }
+    for(let i =0;i<importTest.length;i++){
+        let request = {
+            "schemaName":importTest[i].schemaName,
+            "versionUUID":uuidV1,
+            "condition":importTest[i].value
+        }
+        await axios.post('/data/search-data',request).then((data)=>{
+            if(data.data.rows.length === 0 )throw data
+            importTest[i].uuid = data.data.rows[0]._uuid
+        }).catch((data)=>{
+            console.log("XXXXXsearch-data Fail")
+            console.log(request)
+        });  
+    }
+}
+async function Run_linkData(){
+    for(let i =0;i<createDataLink.length;i++){
+        let request = {
+            "node1":importTest[createDataLink[i][0]].schemaName,
+            "node2":importTest[createDataLink[i][1]].schemaName,
+            "uuid1":importTest[createDataLink[i][0]].uuid,
+            "uuid2":importTest[createDataLink[i][1]].uuid,
+            "version":uuidV1
+        }
+        await axios.post('/link/data-link',request).then((data)=>{
+            
+        }).catch((data)=>{
+            console.log("XXXXXdata-link Fail")
+            console.log(data.response.data)
+            console.log(request)
+        });  
+    }
+}
+async function runTest(){
+    await Run_register()
+    await Run_login()
+    await Run_createVersion1()
+    await Run_createSchema()    
+    if(!runall)name = readline.question("Continues?");
+    let realData = "n"
+    if(!runall)realData = readline.question("import real data(y/n)?");
+    if(realData === 'y'){
+    }else{
+        await Run_importTest()
+    }
+    await Run_creatMetaLink()
 
-
+    await Run_TestSearch()
+    await Run_linkData()
 /////////////////////////////////////////////////////////////////////////////
     let resetAll = readline.question("resetAll data(y/n)?");
     if(resetAll ==='y'){
