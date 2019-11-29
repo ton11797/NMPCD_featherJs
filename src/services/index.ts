01dcfcbed636a229b234c37c6b672ac6ddade97b
pongsatorn <ton11797@hotmail.com>
2019-11-29 21:24:02 +0700

    new route

--- src/services/index.ts
+++ src/services/index.ts
@@ -13,6 +13,13 @@ import linkMetaLink from './link/meta-link/meta-link.service';
 import linkDataLink from './link/data-link/data-link.service';
 import dataSearchData from './data/search-data/search-data.service';
 import dataInsertConfirm from './data/insert-confirm/insert-confirm.service';
+import confirmConfirmData from './confirm/confirm-data/confirm-data.service';
+import confirmConfirmGet from './confirm/confirm-get/confirm-get.service';
+import confirmConfirmLink from './confirm/confirm-link/confirm-link.service';
+import dataDeleteData from './data/delete-data/delete-data.service';
+import dataEditData from './data/edit-data/edit-data.service';
+import dataEditConfirm from './data/edit-confirm/edit-confirm.service';
+import dataDeleteConfirm from './data/delete-confirm/delete-confirm.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application) {
@@ -30,4 +37,11 @@ export default function (app: Application) {
   app.configure(linkDataLink);
   app.configure(dataSearchData);
   app.configure(dataInsertConfirm);
+  app.configure(confirmConfirmData);
+  app.configure(confirmConfirmGet);
+  app.configure(confirmConfirmLink);
+  app.configure(dataDeleteData);
+  app.configure(dataEditData);
+  app.configure(dataEditConfirm);
+  app.configure(dataDeleteConfirm);
 }
