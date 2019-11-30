66727d78a993cac67ef1e96f1879869218779985
pongsatorn <ton11797@hotmail.com>
2019-11-30 22:23:47 +0700

    edit and delete with confirm

--- src/services/data/delete-confirm/delete-confirm.class.ts
+++ src/services/data/delete-confirm/delete-confirm.class.ts
@@ -1,7 +1,11 @@
 import { Id, NullableId, Paginated, Params, ServiceMethods } from '@feathersjs/feathers';
 import { Application } from '../../../declarations';
-
-interface Data {}
+import { BadRequest } from '@feathersjs/errors';
+interface Data {
+  versionUUID:string,
+  schemaName:string,
+  uuid:string
+}
 
 interface ServiceOptions {}
 
@@ -18,18 +22,34 @@ export class DeleteConfirm implements ServiceMethods<Data> {
     return [];
   }
 
-  async get (id: Id, params?: Params): Promise<Data> {
+  async get (id: Id, params?: Params): Promise<any> {
     return {
       id, text: `A new message with ID: ${id}!`
     };
   }
 
-  async create (data: Data, params?: Params): Promise<Data> {
+  async create (data: Data|Data[], params?: Params): Promise<any> {
     if (Array.isArray(data)) {
       return Promise.all(data.map(current => this.create(current, params)));
     }
-
-    return data;
+    let debug = this.app.get('debug')
+    debug.logging(1,"API_call","delete-confirm") 
+    let {versionUUID,schemaName,uuid} = data
+    let result = await (await this.app.get('mongoClient')).collection("Schema").findOne({versionUUID})
+    if(result === null)throw new BadRequest("versionUUID not found")
+    if(result.schema[schemaName] === undefined)throw new BadRequest("schemaName not found")
+    let column = ""
+    for(let i=0;i<result.schema[schemaName].length;i++){
+      column = `${column} ,${result.schema[schemaName][i].fieldName} `
+    }
+    let sql = `INSERT INTO "${schemaName}_${versionUUID}_c" (_uuid${column},_count,_user,_action,_status)
+    SELECT _uuid${column} ,0 as _count ,'{}' as _user, 2 as _action ,0 as _status FROM "${schemaName}_${versionUUID}"
+    WHERE _uuid = '${uuid}'
+    `
+    debug.logging(99,"data-link","postgres "+sql)
+    let client:any = await this.app.get('postgresClient')
+    await client.query(sql) 
+    return {result:"success"};
   }
 
   async update (id: NullableId, data: Data, params?: Params): Promise<Data> {
@@ -40,7 +60,7 @@ export class DeleteConfirm implements ServiceMethods<Data> {
     return data;
   }
 
-  async remove (id: NullableId, params?: Params): Promise<Data> {
+  async remove (id: NullableId, params?: Params): Promise<any> {
     return { id };
   }
 }
--- src/services/data/edit-confirm/edit-confirm.class.ts
+++ src/services/data/edit-confirm/edit-confirm.class.ts
@@ -1,7 +1,12 @@
 import { Id, NullableId, Paginated, Params, ServiceMethods } from '@feathersjs/feathers';
 import { Application } from '../../../declarations';
-
-interface Data {}
+import { BadRequest } from '@feathersjs/errors';
+interface Data {
+  versionUUID:string,
+  schemaName:string,
+  uuid:string,
+  data:object
+}
 
 interface ServiceOptions {}
 
@@ -18,18 +23,41 @@ export class EditConfirm implements ServiceMethods<Data> {
     return [];
   }
 
-  async get (id: Id, params?: Params): Promise<Data> {
+  async get (id: Id, params?: Params): Promise<any> {
     return {
       id, text: `A new message with ID: ${id}!`
     };
   }
 
-  async create (data: Data, params?: Params): Promise<Data> {
+  async create (data: Data|Data[], params?: Params): Promise<any> {
     if (Array.isArray(data)) {
       return Promise.all(data.map(current => this.create(current, params)));
     }
-
-    return data;
+    let debug = this.app.get('debug')
+    debug.logging(1,"API_call","edit-confirm") 
+    let {versionUUID,schemaName,uuid} = data
+    let updateData:any = data.data
+    let result = await (await this.app.get('mongoClient')).collection("Schema").findOne({versionUUID})
+    if(result === null)throw new BadRequest("versionUUID not found")
+    if(result.schema[schemaName] === undefined)throw new BadRequest("schemaName not found")
+    let column = ""
+    let columnUpdate = ""
+    for(let i=0;i<result.schema[schemaName].length;i++){
+      if(updateData[result.schema[schemaName][i].fieldName] !== undefined){
+        columnUpdate = `${columnUpdate} ,'${updateData[result.schema[schemaName][i].fieldName]}' as ${result.schema[schemaName][i].fieldName} `
+      }else{
+        columnUpdate = `${columnUpdate} ,${result.schema[schemaName][i].fieldName} `
+      }
+      column = `${column} ,${result.schema[schemaName][i].fieldName} `
+    }
+    let sql = `INSERT INTO "${schemaName}_${versionUUID}_c" (_uuid${column},_count,_user,_action,_status)
+    SELECT _uuid${columnUpdate} ,0 as _count ,'{}' as _user, 1 as _action ,0 as _status FROM "${schemaName}_${versionUUID}"
+    WHERE _uuid = '${uuid}'
+    `
+    debug.logging(99,"data-link","postgres "+sql)
+    let client:any = await this.app.get('postgresClient')
+    await client.query(sql) 
+    return {result:"success"};
   }
 
   async update (id: NullableId, data: Data, params?: Params): Promise<Data> {
@@ -40,7 +68,7 @@ export class EditConfirm implements ServiceMethods<Data> {
     return data;
   }
 
-  async remove (id: NullableId, params?: Params): Promise<Data> {
+  async remove (id: NullableId, params?: Params): Promise<any> {
     return { id };
   }
 }
